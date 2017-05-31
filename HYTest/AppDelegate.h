f0b5f573465ebcfaff897424a3fc108c6836deff
huyang <982189800@qq.com>
2017-05-31 09:52:54 +0800

    init

---
+++ HYTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYTest
+//
+//  Created by 胡杨 on 2017/5/31.
+//  Copyright © 2017年 Hoo.c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYTest
+//
+//  Created by 胡杨 on 2017/5/31.
+//  Copyright © 2017年 Hoo.c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
